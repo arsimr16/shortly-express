83be2ba9ab92d43e3635ce2990a9b18960b880f2
arsimr16 <arsimr16@g.holycross.edy>
2017-09-21 15:42:57 -0700

    create new cookie when one does not exist

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,11 +2,33 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
-  models.Sessions.create();
-  next();
+  if (Object.keys(req.cookies).length === 0) {
+    models.Sessions.create()
+    .then((sessionId) => {
+      var insertId = sessionId.insertId;
+      models.Sessions.get({id: insertId})
+      .then((userInfo) => {
+        req.session = userInfo;
+        res.cookies['shortlyid'] = {};
+        res.cookies['shortlyid'].value = req.session.hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
 };
+    //console.log('session', req.session);
+    //console.log('session hash', req.session.hash);
+   
+  
+  // if (!req.cookies.keys) {
+  //   //create new session
+  //   models.Sessions.create();
+  // }
+  //models.Sessions.create();
+
 
 /************************************************************/
 // Add additional authentication middleware functions below
-/************************************************************/
-
+//************************************************************/
