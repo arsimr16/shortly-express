2ddb2d11e11fb3d889674e25f7dc251c5a493592
arsimr16 <arsimr16@g.holycross.edy>
2017-09-21 17:00:46 -0700

    assign session property on request

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,7 +2,7 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
-  if (Object.keys(req.cookies).length === 0) {
+  if (!req.cookies.shortlyid) {
     models.Sessions.create()
     .then((sessionId) => {
       var insertId = sessionId.insertId;
@@ -15,7 +15,13 @@ module.exports.createSession = (req, res, next) => {
       });
     });
   } else {
-    next();
+    // req.session = {};
+    //req.session.hash = req.cookies.shortlyid;
+    models.Sessions.get({hash: req.cookies.shortlyid})
+    .then((sessionsData) => {
+      req.session = sessionsData;
+      next();
+    });
   }
 };
     //console.log('session', req.session);
